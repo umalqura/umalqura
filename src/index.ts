10ccbb1debcd30c0d03ff3315ee4b807bb6177e5
Alaa Masoud <alaa.masoud@live.com>
2019-07-03 21:33:02 +0300

    Do not export DatePart

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import DatePart from './DatePart';
 import UmAlQuraCalendar from './UmAlQuraCalendar';
 
 export const VERSION: string = "0.0.0-DEV_BUILD";
-export { DatePart, UmAlQuraCalendar };
+export { UmAlQuraCalendar };
