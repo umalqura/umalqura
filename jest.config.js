da04026563a9dc4a01829ed1c668e59a54e05929
Alaa Masoud <alaa.masoud@live.com>
2019-07-07 01:42:50 +0300

    Simplify webapck

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
         }
     },
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'node'
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,59 @@
 const path = require('path');
 const webpack = require('webpack');
+const pkg = require(path.resolve(__dirname, 'package.json'));
+const browserBaseName = 'hijri';
 
-module.exports = wp(__dirname, 'hijri', {
-
-});
-
-function wp(modulePath, browserBaseName, options) {
-    const pkg = require(path.resolve(modulePath, 'package.json'));
-
-    options = options || {};
-
-    return {
-        entry: path.resolve(modulePath, 'src', 'index.ts'),
-        mode: 'none',
-        node: {
-            global: true,
-            process: false,
-            Buffer: false,
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    mode: 'none',
+    node: {
+        global: true,
+        process: false,
+        Buffer: false,
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    configFile: path.resolve(__dirname, 'tsconfig.json'),
+                },
+            }, ],
+            exclude: /node_modules/,
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: `${browserBaseName}.js`,
+        path: path.resolve(__dirname, 'dist', 'browser'),
+        library: {
+            root: pkg.umd_name.split('.'),
+            amd: pkg.umd_name,
         },
-        module: {
-            rules: [{
-                test: /\.ts$/,
-                use: [{
-                    loader: 'ts-loader',
-                    options: {
-                        configFile: path.resolve(modulePath, 'tsconfig.json'),
-                    },
-                }, ],
-                exclude: /node_modules/,
-            }]
-        },
-        resolve: {
-            extensions: ['.ts', '.js'],
-            alias: options.alias,
-        },
-        output: {
-            filename: `${browserBaseName}.js`,
-            path: path.resolve(modulePath, 'dist', 'browser'),
-            library: {
-                root: pkg.umd_name.split('.'),
-                amd: pkg.umd_name,
-            },
-            libraryTarget: 'umd',
-        },
-        plugins: [
-            new webpack.SourceMapDevToolPlugin({
-                filename: `${browserBaseName}.js.map`,
-                moduleFilenameTemplate(info) {
-                    let resourcePath = info.resourcePath;
+        libraryTarget: 'umd',
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: `${browserBaseName}.js.map`,
+            moduleFilenameTemplate(info) {
+                let resourcePath = info.resourcePath;
 
-                    // Clean up the source map urls.
-                    while (resourcePath.startsWith('./') || resourcePath.startsWith('../')) {
-                        if (resourcePath.startsWith('./')) {
-                            resourcePath = resourcePath.substring(2);
-                        } else {
-                            resourcePath = resourcePath.substring(3);
-                        }
+                // Clean up the source map urls.
+                while (resourcePath.startsWith('./') || resourcePath.startsWith('../')) {
+                    if (resourcePath.startsWith('./')) {
+                        resourcePath = resourcePath.substring(2);
+                    } else {
+                        resourcePath = resourcePath.substring(3);
                     }
-
-                    // We embed the sources so we can falsify the URLs a little, they just
-                    // need to be identifiers that can be viewed in the browser.
-                    return `webpack://${pkg.umd_name}/${resourcePath}`;
                 }
-            }),
-        ],
-        externals: options.externals,
-    };
-}
+
+                // We embed the sources so we can falsify the URLs a little, they just
+                // need to be identifiers that can be viewed in the browser.
+                return `webpack://${pkg.umd_name}/${resourcePath}`;
+            }
+        }),
+    ],
+};
