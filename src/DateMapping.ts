4cd34d75385fe92ffeedea9811427c9f70c170e9
Alaa Masoud <alaa.masoud@live.com>
2019-07-07 16:45:45 +0300

    Reorder fields

--- src/DateMapping.ts
+++ src/DateMapping.ts
@@ -1,11 +1,11 @@
 class DateMapping {
+    public hijriMonthsLengthFlags: number;
+    public gregorianDate: Date;
+
     constructor(monthsLengthFlags: number, gy: number, gm: number, gd: number) {
         this.hijriMonthsLengthFlags = monthsLengthFlags;
         this.gregorianDate = new Date(gy, gm, gd);
     }
-
-    public hijriMonthsLengthFlags: number;
-    public gregorianDate: Date;
 }
 
 export default DateMapping;
