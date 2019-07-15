dc6c912bde4ec7d57794c64be7b0d2d5c6f0528d
Alaa Masoud <alaa.masoud@live.com>
2019-07-15 22:03:30 +0300

    Move tsconfig.jest to tests folder

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     globals: {
         "ts-jest": {
-            tsConfig: "./tsconfig.jest.json",
+            tsConfig: "./tests/tsconfig.jest.json",
         }
     },
     preset: 'ts-jest',
